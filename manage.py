0e6ee2a5aa1b646686afe9940699f19ba2ca990e
小爆虾 <xiaobaoxia@xiaobaoxia.local>
2017-12-04 23:49:45 +0800

    晚上的更改

--- manage.py
+++ manage.py
@@ -1,7 +1,16 @@
 # coding=utf-8
 from flask import Flask
+from flask_script import Manager
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate, MigrateCommand
+from config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
+manager = Manager(app)
+db = SQLAlchemy(app)
+Migrate(app, db)
+manager.add_command('db', MigrateCommand)
 
 
 @app.route('/')
