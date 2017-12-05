9ca6649f53d595a85e1c48568bc4c6c36f26f4db
小爆虾 <xiaobaoxia@xiaobaoxia.local>
2017-12-05 14:47:56 +0800

    第二次完整提交

---
+++ config.py
@@ -0,0 +1,32 @@
+# coding=utf-8
+import redis
+
+
+class Config(object):
+    SECRET_KEY = 'Alj2oWAjhqKiOIM9jPOJkSR4KNea8igzZde7oGdPuoGRuIaQ3a12wekqJqG2FP5l'
+    # MySQL配置
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:mysql@192.168.191.138:3306/ihome'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # Redis配置
+    REDIS_HOST = '192.168.191.138'
+    REDIS_PORT = 6379
+
+    # session配置
+    SESSION_TYPE = 'redis'
+    SESSION_REDIS = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT)
+    SESSION_USE_SIGNER = True
+    PERMANENT_SESSION_LIFETIME = 86400
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+
+
+class ProductionConfig(Config):
+    pass
+
+
+config = {
+    'DevelopmentConfig': DevelopmentConfig,
+    'ProductionConfig': ProductionConfig
+}
---
+++ iHome/__init__.py
@@ -0,0 +1,26 @@
+# coding=utf-8
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf.csrf import CSRFProtect
+from flask_session import Session
+import redis
+from config import config
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+
+db = SQLAlchemy()
+csrf = CSRFProtect()
+redis_store = None
+
+
+def create_app(config_name):
+    app = Flask(__name__)
+    app.config.from_object(config[config_name])
+    Session(app)
+    db.init_app(app)
+    csrf.init_app(app)
+    global redis_store
+    redis_store = redis.StrictRedis(host=config[config_name].REDIS_HOST, port=config[config_name].REDIS_PORT)
+    return app
