bfad4b487b5787d352a2c7777fd93e2ddb62abbe
小爆虾 <xiaobaoxia@xiaobaoxia.local>
2017-12-05 14:07:23 +0800

    改版提交第一次

--- manage.py
+++ manage.py
@@ -1,22 +1,41 @@
 # coding=utf-8
-from flask_script import Manager
-from flask_migrate import Migrate, MigrateCommand
-from iHome import create_app, db
-from flask import session
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf.csrf import CSRFProtect
+from flask_session import Session
+import redis
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
-app = create_app('DevelopmentConfig')
-from iHome import redis_store
-manager = Manager(app)
-Migrate(app, db)
-manager.add_command('db', MigrateCommand)
+
+class Config(object):
+    SECRET_KEY = 'Alj2oWAjhqKiOIM9jPOJkSR4KNea8igzZde7oGdPuoGRuIaQ3a12wekqJqG2FP5l'
+    DEBUG = True
+    # MySQL配置
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:mysql@192.168.191.138:3306/ihome'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # Redis配置
+    REDIS_HOST = '192.168.191.138'
+    REDIS_PORT = 6379
+
+    # session配置
+
+
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+CSRFProtect(app)
+redis_store = redis.StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
 
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
-    redis_store.set('name', '222')
-    session['name'] = '111'
-    return 'hello world'
-
+    redis_store.set('name', 'asd')
+    return 'index'
 
 if __name__ == '__main__':
-    manager.run()
+    app.run()
